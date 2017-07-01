008f79c810f5d5e3e71871b5e87d403f804550ba
rilane <s10474@pjwstk.edu.pl>
2017-07-01 07:34:57 +0200

    added map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -3,16 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
+    // pacz w ustawienia tekstury!
+    public Texture2D mapBlueprint;
+    public bool spawnInLocalCoordinates;
+    public MapEntry[] mapInput;
 
-	
-    public static bool canPass()
+    private static Map instance;
+    private Dictionary<string, GameObject> mapDict;
+
+    void Start()
     {
-        return true;
+        instance = this;
+        GenerateDict();
+        GenerateMap();
     }
 
-    public static bool canSee()
+    private void GenerateDict()
     {
-        return true;
+        mapDict = new Dictionary<string, GameObject>();
+        foreach (MapEntry entry in mapInput)
+            mapDict.Add(toColorString(entry.color), entry.prefab);
+    }
+
+    private void GenerateMap()
+    {
+        for (int x = 0; x < mapBlueprint.width; x++)
+            for (int y = 0; y < mapBlueprint.height; y++)
+                spawnCube(x, y);
+    }
+
+    private void spawnCube(int x, int y)
+    {
+        Color cubeCode = mapBlueprint.GetPixel(x, y);
+        
+        if (mapDict.ContainsKey(toColorString(cubeCode)))
+        {
+            Vector3 position = new Vector3(x, y,0);
+            GameObject tile = Instantiate(mapDict[toColorString(cubeCode)], this.transform.localPosition + position, Quaternion.identity, this.transform);
+            Debug.Log("Instantialing "+ toColorString(cubeCode) + " " + x + " + " + y);
+        }
+        Debug.Log(canPass(x, y));
+    }
+
+    private string toColorString(Color color)
+    {
+        return color.r.ToString("F") + color.g.ToString("F") + color.b.ToString("F");
+    }
+
+
+    public static bool canPass(int x, int y)
+    {
+        return instance.mapBlueprint.GetPixel(x, y).r == 1f;
+    }
+
+    public static bool canSee(int x, int y)
+    {
+        return instance.mapBlueprint.GetPixel(x, y).g == 1f;
     }
 
 }
