1764665f964eecb5708204e70455f985ffa49524
Winxorz <winxorz@gmail.com>
2017-07-01 14:29:32 +0200

    Changed player movement to GetKeyDown, eliminated bugs that caused the player to go off the grid. Created a Player prefab. Added my test scene to the repository.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,54 +8,72 @@ public class PlayerMovement : MonoBehaviour
     public float pMovementDuration;
 
     private Vector3 mMovementVector;
-    private float mTimeCounter;
+    private bool mIsMoving;
 
 
     // Use this for initialization
     void Start()
     {
-        mTimeCounter = 1.0f;
         pMovementDuration = 0.1f;
+        mIsMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mTimeCounter += Time.deltaTime;
-
-        if (mTimeCounter >= pMovementDuration*3)
-        {
-            MovementInput();
-        }
+        MovementInput();
     }
 
     private void MovementInput()
     {
-        mTimeCounter = 0.0f;
 
-        mMovementVector.Set((Input.GetAxisRaw("Horizontal")), Input.GetAxisRaw("Vertical"), 0);
-
-        // Checks if mMovementVector doesn't imply diagonal movement, checks if tile moved to is passable terrain, then starts moving the player.
-        if ((mMovementVector.x == 0 && mMovementVector.y != 0)
-            || (mMovementVector.x != 0 && mMovementVector.y == 0))
+        if (!mIsMoving)
         {
-            //if(checkIfPassable())
-            //{
-            //PlayAnimation()
-            Debug.Log(mMovementVector);
-            StartCoroutine(MovePlayer(transform.localPosition, transform.localPosition + mMovementVector, pMovementDuration));
-            //}
-            //else
-            //{
-            //
-        }
 
+            if (Input.GetKey("left"))
+            {
+                mMovementVector = (Vector3.left);
+            }
+            if (Input.GetKey("right"))
+            {
+                mMovementVector = (Vector3.right);
+            }
+            if (Input.GetKey("up"))
+            {
+                mMovementVector = (Vector3.up);
+            }
+            if (Input.GetKey("down"))
+            {
+                mMovementVector = (Vector3.down);
+            }
+
+            // Checks if mMovementVector doesn't imply diagonal movement, checks if tile moved to is passable terrain, then starts moving the player.
+            if ((mMovementVector.x == 0 && mMovementVector.y != 0)
+                || (mMovementVector.x != 0 && mMovementVector.y == 0))
+            {
+                //if(checkIfPassable())
+                //{
+                //PlayAnimation()
+                Debug.Log(mMovementVector);
+                mIsMoving = true;
+                StartCoroutine(MovePlayer(transform.localPosition, transform.localPosition + mMovementVector, pMovementDuration));
+                //ChangePlayerFacing();
+                //}
+                //else
+                //{
+                //ChangePlayerFacing();
+                //}
+                //mTimeCounter = 0.0f;
+            }
+
+            mMovementVector.Set(0, 0, 0);
+        }
     }
 
     //Interpolates the move and ensures smooth transition between coordinates
     IEnumerator MovePlayer(Vector3 startingPosition, Vector3 endingPosition, float moveDuration)
     {
-        int steps = (int) Mathf.Ceil(moveDuration * 60);
+        int steps = (int)Mathf.Ceil(moveDuration * 60);
 
         float stepDuration = moveDuration / steps;
 
@@ -70,6 +88,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         transform.localPosition = endingPosition;
+        mIsMoving = false;
     }
 
     /* TODO: Implement a check whether the tile to be moved to is passable terrain or not.
