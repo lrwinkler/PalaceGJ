6dfc5f7567a8c124de3e460a88b518e6bf4ae9d7
rilane <s10474@pjwstk.edu.pl>
2017-07-02 11:58:43 +0200

    added random tiles

---
+++ Assets/Scripts/RandomTexture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTexture : MonoBehaviour {
+
+    public List<Sprite> sprites;
+
+	// Use this for initialization
+	void Start () {
+        if (Random.Range(0, 5) == 0)
+        {
+            gameObject.GetComponentInChildren<SpriteRenderer>().sprite = sprites[Random.Range(1, sprites.Count)];
+        }
+    }
+	
+}
